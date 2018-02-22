b085fcf706629bbc1bd445101951c2edc297776c
Jeff <jeffreylehman94@gmail.com>
2018-02-22 16:59:23 -0500

    Large majority of the program: Threads and MainActivity Seems to be working

--- app/src/main/java/major_assignment_1/tcp_port_scanner/MainActivity.java
+++ app/src/main/java/major_assignment_1/tcp_port_scanner/MainActivity.java
@@ -2,30 +2,44 @@ package major_assignment_1.tcp_port_scanner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static int NUMBER_OF_THREADS = 100;
     private String IP = null;
-    protected static ArrayList<Integer> Open_Ports = new ArrayList();
+    protected static ArrayList<Integer> OPEN_PORTS = new ArrayList();
     private int MAX_PORTS_TO_SCAN = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startScan("2601:8a:8100:b885:f142:c96:cfea:64fc");
 
     }
-    private void startScan(String IP){
-        Open_Ports = new ArrayList<Integer>();
+
+    private void startScan(String IP) {
+        OPEN_PORTS = new ArrayList<Integer>();
         ArrayList<TCP_Thread> threads = new ArrayList();
-        for(int i=0;i<16;i++){
-            threads.add(new TCP_Thread(IP));
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            threads.add(new TCP_Thread(IP, MAX_PORTS_TO_SCAN, i));
+            Log.d("THREAD", i + " CREATED");
         }
-        for(TCP_Thread thread:threads){
+        for(TCP_Thread thread : threads){
             thread.start();
+        }
+        for(TCP_Thread thread : threads){
+            try{thread.join();}
+            catch(Exception e){
+
+            }
+        }
+        for(int port: OPEN_PORTS) {
+            Log.d("FINISH OPEN PORT AT", Integer.toString(port));
         }
     }
 }
--- app/src/main/java/major_assignment_1/tcp_port_scanner/TCP_Thread.java
+++ app/src/main/java/major_assignment_1/tcp_port_scanner/TCP_Thread.java
@@ -8,25 +8,26 @@ import android.util.Log;
 
 public class TCP_Thread extends Thread{
     // TODO CHANGE PORT AND CREATE PROPER CONSTRUCTOR
-    private int CURR_PORT = 5;
-    Object[] objects = {0};
     private String IP = null;
-    private final int THREAD_NUMBER = 0;
+    private int PORT_INCREMENT = 0;
+    private int THREAD_NUMBER = 0;
 
-    TCP_Thread (String ip, int thread){
-
+    TCP_Thread (String ip, int portsMax, int thread){
+        IP = ip;
+        PORT_INCREMENT = portsMax/MainActivity.NUMBER_OF_THREADS;
+        THREAD_NUMBER = thread;
 
     }
     public void run() {
-        int port = CURR_PORT;
-        CURR_PORT += 10;
-        for (int i = port; i < port + 10; i++) {
-            Log.d("RUN", port / 10 + " scanning " + i);
+        int startingPort = PORT_INCREMENT*THREAD_NUMBER;
+        Object[] objects = {0};
+        for (int i = startingPort; i < startingPort + PORT_INCREMENT; i++) {
             objects[0] = i;
             try {
+                Log.d("THREAD", "scanning " +i);
                 Scan scan = new Scan(IP);
                 if (scan.doInBackground(objects)) {
-                    MainActivity.Open_Ports.add(i);
+                    MainActivity.OPEN_PORTS.add(i);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
