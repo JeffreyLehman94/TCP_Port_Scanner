da7c7b7d4cf27db84f5119e3b08ad666977f60b6
Jeff <jeffreylehman94@gmail.com>
2018-03-01 23:11:47 -0500

    Started on buttons

--- app/src/main/java/major_assignment_1/tcp_port_scanner/MainActivity.java
+++ app/src/main/java/major_assignment_1/tcp_port_scanner/MainActivity.java
@@ -1,8 +1,11 @@
 package major_assignment_1.tcp_port_scanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -13,12 +16,22 @@ public class MainActivity extends AppCompatActivity {
     private String IP = null;
     protected static ArrayList<Integer> OPEN_PORTS = new ArrayList();
     private int MAX_PORTS_TO_SCAN = 1000;
+    private Button bStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startScan("2601:8a:8100:b885:f142:c96:cfea:64fc");
+        bStart = (Button) findViewById(R.id.BUTTON_START);
+
+        bStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO ASSIGN INFO
+                // TODO START SCAN
+            }
+        });
 
     }
 
